24351914713a94bff1837fa4678deadfc73412c1
blair drummond <blair.robert.drummond@gmail.com>
2016-01-27 10:25:01 -0500

    htop

--- initialize.sh
+++ initialize.sh
@@ -5,7 +5,7 @@ pause(){
 }
 
 # Install (no window manager yet)
-sudo pacman -S adobe-source-code-pro-fonts arandr aspell aspell-en aspell-fr curl dfc dmenu emacs feh hunspell networkmanager networkmanager-openvpn openssh openvpn pandoc pygmentize python-pip python-pyflakes python-pygments python-virtualenv ranger rsync rxvt-unicode screenfetch texlive-bibtexextra texlive-core texlive-formatsextra texlive-latexextra texlive-pictures texlive-plainextra texlive-science ttf-dejavu unzip urxvt-perls wget wireless_tools wpa_supplicant xbindkeys xf86-input-synaptics xfce4-screenshooter xorg-server xorg-xclipboard xorg-xinit xorg-xmodmap xorg-xsetroot zathura zathura-pdf-poppler zathura-ps zip zsh zsh-completions
+sudo pacman -S adobe-source-code-pro-fonts arandr aspell aspell-en aspell-fr curl dfc dmenu emacs feh htop hunspell networkmanager networkmanager-openvpn openssh openvpn pandoc pygmentize python-pip python-pyflakes python-pygments python-virtualenv ranger rsync rxvt-unicode screenfetch texlive-bibtexextra texlive-core texlive-formatsextra texlive-latexextra texlive-pictures texlive-plainextra texlive-science ttf-dejavu unzip urxvt-perls wget wireless_tools wpa_supplicant xbindkeys xf86-input-synaptics xfce4-screenshooter xorg-server xorg-xclipboard xorg-xinit xorg-xmodmap xorg-xsetroot zathura zathura-pdf-poppler zathura-ps zip zsh zsh-completions
 
 # Video card
 sudo pacman -S xf86-video-nouveau
